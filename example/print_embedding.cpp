7a5969dfff43264fcdc7cc9cf9e30d4e408d99c2
rhalbersma <rhalbersma@gmail.com>
2024-06-17 12:48:49 +0200

    Adapt to std::print

--- example/print_embedding.cpp
+++ example/print_embedding.cpp
@@ -3,15 +3,14 @@
 //    (See accompanying file LICENSE_1_0.txt or copy at
 //          http://www.boost.org/LICENSE_1_0.txt)
 
-#define FMT_HEADER_ONLY
-#include <fmt/core.h>
-#include <fmt/ranges.h>
-
 #include <tabula/board.hpp>     // basic_board
 #include <tabula/format.hpp>    // print
 #include <tabula/games.hpp>     // chess, draughts, stratego
 #include <tabula/padding.hpp>   // padding, right_padding
-#include <tabula/tuple.hpp>     // for_each, enumerate
+#include <tabula/tuple.hpp>     // for_each
+#include <fmt/format.h>
+#include <fmt/ranges.h>
+#include <print>                // print, println
 #include <tuple>                // tuple
 
 int main()
@@ -66,18 +65,18 @@ int main()
         );
 
         for_each(boards, [](auto b) {
-                fmt::print("{}\n", b);
-                fmt::print("config: W = {}, H = {}", b.width, b.height);
+                std::println("{}\n", b);
+                std::print("config: W = {}, H = {}", b.width, b.height);
                 if constexpr (b.is_chequered) {
-                        fmt::print(", P = {}", b.parity());
+                        std::print(", P = {}", b.parity());
                 }
-                fmt::print(", size = {}\n", b.size);
-                fmt::print("padded: W = {}, H = {}", b.padded_width, b.padded_height);
+                std::println(", size = {}", b.size);
+                std::print("padded: W = {}, H = {}", b.padded_width, b.padded_height);
                 if constexpr (b.is_chequered) {
-                        fmt::print(", P = {}", b.padded_parity());
+                        std::print(", P = {}", b.padded_parity());
                 }
-                fmt::print(", size = {} ({})\n", b.padded_size, b.valid_range);
-                fmt::print("directional strides: {}\n", b.strides);
-                fmt::print("{:->40}\n", "");
+                std::println(", size = {} ({})", b.padded_size, b.valid_range);
+                fmt::println("directional strides: {}", b.strides);
+                std::println("{:->40}", "");
         });
 }
--- include/tabula/format.hpp
+++ include/tabula/format.hpp
@@ -5,53 +5,30 @@
 //    (See accompanying file LICENSE_1_0.txt or copy at
 //          http://www.boost.org/LICENSE_1_0.txt)
 
-#define FMT_HEADER_ONLY
-#include <fmt/format.h>
-
-#include <tabula/board.hpp>
+#include <tabula/board.hpp>     // basic_board
 #include <tabula/padding.hpp>   // padding
+#include <format>               // formater, format_to
 #include <ranges>               // iota, reverse
 
 template<class Grid, tabula::padding Padding>
-struct fmt::formatter<tabula::basic_board<Grid, Padding>>
+struct std::formatter<tabula::basic_board<Grid, Padding>>
 {
-        // Presentation format: 'f' - fixed, 'e' - exponential.
-        char presentation = 'f';
-
-        // Parses format specifications of the form ['f' | 'e'].
-        constexpr auto parse(format_parse_context& ctx)
+        constexpr auto parse(auto& ctx)
         {
-                // auto parse(format_parse_context &ctx) -> decltype(ctx.begin()) // c++11
-                // [ctx.begin(), ctx.end()) is a character range that contains a part of
-                // the format string starting from the format specifications to be parsed,
-                // e.g. in
-                //
-                //   fmt::format("{:f} - point of interest", point{1, 2});
-                //
-                // the range will contain "f} - point of interest". The formatter should
-                // parse specifiers until '}' or the end of the range. In this example
-                // the formatter should parse the 'f' specifier and return an iterator
-                // pointing to '}'.
-
-                // Return an iterator past the end of the parsed range:
-                // TODO!!
-                return ctx.end();
+                return ctx.begin();
         }
 
-        // Formats the point p using the parsed format specification (presentation)
-        // stored in this formatter.
-        template<typename FormatContext>
-        auto format(tabula::basic_board<Grid, Padding> const& board, FormatContext& ctx)
+        auto format(tabula::basic_board<Grid, Padding> const& board, auto& ctx) const
         {
                 for (auto rank : std::views::iota(0, board.height) | std::views::reverse) {
                         for (auto file : std::views::iota(0, board.width)) {
                                 if (auto const square = board.square(file, rank); square.is_valid()) {
-                                        format_to(ctx.out(), "{:>4}", board.padded(square));
+                                        std::format_to(ctx.out(), "{:>4}", board.padded(square));
                                 } else {
-                                        format_to(ctx.out(), "{:>4}", "");
+                                        std::format_to(ctx.out(), "{:>4}", "");
                                 }
                                 if (file == board.width - 1) {
-                                        format_to(ctx.out(), "\n");
+                                        std::format_to(ctx.out(), "\n");
                                 }
                         }
                 }
