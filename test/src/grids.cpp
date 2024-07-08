e56e07ab0f2ced9b49f999a312792a1cbe8457eb
rhalbersma <rhalbersma@gmail.com>
2024-07-08 08:58:17 +0200

    Use tuple_cat to group grids

--- test/src/grids.cpp
+++ test/src/grids.cpp
@@ -11,70 +11,11 @@
 #include <boost/test/unit_test.hpp>     // BOOST_AUTO_TEST_SUITE, BOOST_AUTO_TEST_SUITE_END, BOOST_AUTO_TEST_CASE_TEMPLATE, BOOST_CHECK, BOOST_CHECK_EQUAL
 #include <concepts>                     // same_as
 #include <ranges>                       // cartesian_product iota
-#include <tuple>                        // get, tuple, tuple_size_v
+#include <tuple>                        // get, tuple, tuple_cat, tuple_size_v
 
 using namespace tabula;
 
 BOOST_AUTO_TEST_SUITE(Compass)
-
-constexpr auto grids = std::tuple
-{       rectangle{1, 1}
-,       rectangle{1, 2}
-,       rectangle{2, 1}
-,       rectangle{2, 2}
-,       rectangle{2, 3}
-,       rectangle{3, 2}
-,       rectangle{2, 4}
-,       rectangle{4, 2}
-,       rectangle{3, 3}
-,       rectangle{3, 5}
-,       rectangle{5, 3}
-,       chequer{1, 1, 0}
-,       chequer{1, 2, 0}
-,       chequer{2, 1, 0}
-,       chequer{2, 2, 0}
-,       chequer{2, 3, 0}
-,       chequer{3, 2, 0}
-,       chequer{2, 4, 0}
-,       chequer{4, 2, 0}
-,       chequer{3, 3, 0}
-,       chequer{3, 5, 0}
-,       chequer{5, 3, 0}
-,       chequer{1, 1, 1}
-,       chequer{1, 2, 1}
-,       chequer{2, 1, 1}
-,       chequer{2, 2, 1}
-,       chequer{2, 3, 1}
-,       chequer{3, 2, 1}
-,       chequer{2, 4, 1}
-,       chequer{4, 2, 1}
-,       chequer{3, 3, 1}
-,       chequer{3, 5, 1}
-,       chequer{5, 3, 1}
-};
-
-using Indices = boost::mp11::mp_iota_c<std::tuple_size_v<decltype(grids)>>;
-
-BOOST_AUTO_TEST_CASE_TEMPLATE(IndicesAreInvertible, Index, Indices)
-{
-        constexpr auto grid = std::get<Index::value>(grids);
-        for (auto index : std::views::iota(0, grid.size())) {
-                BOOST_CHECK_EQUAL(grid.index(grid.coordinates(index)), index);
-        }
-}
-
-BOOST_AUTO_TEST_CASE_TEMPLATE(CoordinatesOfValidSquaresAreInvertible, Index, Indices)
-{
-        constexpr auto grid = std::get<Index::value>(grids);
-        for (auto coordinates : std::views::cartesian_product(
-                std::views::iota(0, grid.width), 
-                std::views::iota(0, grid.height)
-        )) {
-                if (auto const [ file, rank ] = coordinates; basic_square<grid>{file, rank}.is_valid()) {
-                        BOOST_CHECK(grid.coordinates(grid.index(coordinates)) == coordinates);
-                }
-        }
-}
 
 // Squares (N x N) and odd-sized chequered squares (2N+1 x 2N+1).
 constexpr auto d8_grids = std::tuple
@@ -258,6 +199,38 @@ BOOST_AUTO_TEST_CASE_TEMPLATE(D2RGridsAreInvariantUnderD2R, Index, D2RIndices)
         static_assert(    group::is_invariant(grid, group::d2r));
         static_assert(not group::is_invariant(grid, group::c2d));
         static_assert(not group::is_invariant(grid, group::c2a));
+}
+
+constexpr auto grids = std::tuple_cat
+(       d8_grids
+,       d4c_grids
+,       d4o_grids
+,       d2v_grids
+,       d2h_grids
+,       d2r_grids
+);
+
+using Indices = boost::mp11::mp_iota_c<std::tuple_size_v<decltype(grids)>>;
+
+BOOST_AUTO_TEST_CASE_TEMPLATE(IndicesAreInvertible, Index, Indices)
+{
+        constexpr auto grid = std::get<Index::value>(grids);
+        for (auto index : std::views::iota(0, grid.size())) {
+                BOOST_CHECK_EQUAL(grid.index(grid.coordinates(index)), index);
+        }
+}
+
+BOOST_AUTO_TEST_CASE_TEMPLATE(CoordinatesOfValidSquaresAreInvertible, Index, Indices)
+{
+        constexpr auto grid = std::get<Index::value>(grids);
+        for (auto coordinates : std::views::cartesian_product(
+                std::views::iota(0, grid.width), 
+                std::views::iota(0, grid.height)
+        )) {
+                if (auto const [ file, rank ] = coordinates; basic_square<grid>{file, rank}.is_valid()) {
+                        BOOST_CHECK(grid.coordinates(grid.index(coordinates)) == coordinates);
+                }
+        }
 }
 
 BOOST_AUTO_TEST_SUITE_END()
